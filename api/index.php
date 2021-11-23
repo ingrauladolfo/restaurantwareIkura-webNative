53dae37cea16b19c98c6cafe42a6c968d8654764
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-23 13:55:23 -0600

    require_once('login.php')

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php session_start();
-
-    if(isset($_SESSION['usuarios'])) {
-        header('location: ./principal.php');
-    }else{
-        header('location: ./login.php');
-    }
+    require_once('login.php');
+    // if(isset($_SESSION['usuarios'])) {
+    //     header('location: principal.php');
+    // }else{
+    //     header('location: login.php');
+    // }
 
 
 ?>
