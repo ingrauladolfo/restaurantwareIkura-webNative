20b1ba31adc8f4bde0cbc55be73831c2b71a379d
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-23 13:52:12 -0600

    add now.json

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
-<?php
-require_once __DIR__.'/../index.php';
+<?php session_start();
+
+    if(isset($_SESSION['usuarios'])) {
+        header('location: ./principal.php');
+    }else{
+        header('location: ./login.php');
+    }
+
+
 ?>
