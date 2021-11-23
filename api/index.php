bf27496a1bd170023a42719ba2cdc7af4c75eea9
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-23 14:05:03 -0600

    update index

--- api/index.php
+++ api/index.php
@@ -1,6 +1,8 @@
-<?php session_start();
-    require_once('login.php');
-    require_once('principal.php');
+<?php 
+require_once('login.php');
+require_once('principal.php');
+session_start();
+    
     if(isset($_SESSION['usuarios'])) {
         header('location: principal.php');
     }else{
