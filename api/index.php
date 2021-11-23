2a96d70a805dbc7250bbbfa96581346d8428b1ee
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-23 14:03:08 -0600

    add .htaccess

--- api/index.php
+++ api/index.php
@@ -1,5 +1,6 @@
 <?php session_start();
-    // require_once('login.php');
+    require_once('login.php');
+    require_once('principal.php');
     if(isset($_SESSION['usuarios'])) {
         header('location: principal.php');
     }else{
